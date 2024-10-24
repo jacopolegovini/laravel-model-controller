cf8e1b19c3e1ba90585354829a952cf6f521620d
jacopolegovini <legovini96@gmail.com>
2024-10-24 15:42:50 +0200

    database implemented correctly

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies[0]->title);
+        return view("pages.movies.index");
+    }
 }
---
+++ resources/views/pages/movies/index.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.app")
+
+@section("page-title", "Movies")
+
+@section("main-content")
+<h1>
+    test
+</h1>
+@endsection
